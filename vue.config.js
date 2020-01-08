e211867c269c50d5cedeeb93dab591668951479b
EchoCow <lizhongyue248@163.com>
2020-01-08 15:35:59 +0800

    :heavy_plus_sign:  Adding i18n.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'zh',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
